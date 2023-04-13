d99064aa745e1aadb36f35aceaaeef571d7b6d1e
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-13 14:55:10 +0100

    Updated some views

--- project/website/views.py
+++ project/website/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 posts = [
     {
         'author': 'Michael',
-        'title': "Michael's First Blog Post",
+        'title': "Michael's First Post",
         'content': 'I am a student at the University of Surrey and I am studying Computer Science.',
         'date_posted': 'April 13, 2023'
     },
     {
         'author': 'Nishanth',
-        'title': "Nishanth's First Blog Post",
+        'title': "Nishanth's First Post",
         'content': 'I am a teacher at the University of Surrey and I am teaching Computer Science.',
         'date_posted': 'January 12, 2023'
     },
