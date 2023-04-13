7628231d1b7d08d736bb811b7b91efd562381780
penguin0001 <robynpgreer@gmail.com>
2023-04-13 12:30:53 +0100

    updated urls

--- posts_service/posts_service/settings.py
+++ posts_service/posts_service/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'posts_app.apps.PostsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
