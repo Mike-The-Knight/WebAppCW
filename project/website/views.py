e153be62f2312c6355a4c6c81f4317bb33419872
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-13 11:11:29 +0100

    Updated home page for user feed

--- project/website/views.py
+++ project/website/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Michael',
+        'title': "Michael's First Blog Post",
+        'content': 'First post content',
+        'date_posted': 'April 13, 2023'
+    },
+    {
+        'author': 'Thomas',
+        'title': "Thomas' First Blog Post",
+        'content': 'First post content',
+        'date_posted': 'January 12, 2023'
+    },
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'website/home.html', {})
+    context = {
+        'posts': posts
+    }
+    return render(request, 'website/home.html', context)
