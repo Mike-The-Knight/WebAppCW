a0eb49259bb288ac8d5f6373101323a3dea77970
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-14 15:27:17 +0100

    Updated forms and views for sign up and sign in

--- project/members/views.py
+++ project/members/views.py
@@ -2,31 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def account(request):
     return render(request, 'members/index.html', {'title': 'Account'})
 
 def signup(request):
-    if request.method == "POST":
-        email = request.POST['email']
-        firstname = request.POST['firstname']
-        surname = request.POST['surname']
-        username = request.POST['username']
-        password = request.POST['password']
-        confirmpassword = request.POST['confirmpassword']
-
-        newuser = User.objects.create_user(username, email, password)
-        newuser.first_name = firstname
-        newuser.last_name = surname
-
-        newuser.save()
-
-        messages.success(request, "Your account has been successfully created")
-
-        return redirect('signin')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('signin')
     else:
-        return render(request, 'members/signup.html', {'title': 'Sign up'})
+        form = UserCreationForm()
+    return render(request, 'members/signup.html', {'form': form})
 
 
 def signin(request):
@@ -37,6 +29,7 @@ def signin(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, "Logged in Successfully!")
             return redirect("home")
         else:
             messages.error(request, "Incorrect username or password")
