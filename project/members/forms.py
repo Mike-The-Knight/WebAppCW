878c9cdc239c8b949a745d1ab0a9e6d10929f0b9
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-14 15:49:02 +0100

    Registered User profiles and added to admin

--- project/members/forms.py
+++ project/members/forms.py
@@ -3,10 +3,25 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+#Create the full user update form
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
+
+
+class UserUpdateForm(UserChangeForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
--- project/members/models.py
+++ project/members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+
+    def __str__(self):
+        return self.user.username
