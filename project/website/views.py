31fdd85176be38c9fd138b935a2718450c359185
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-14 10:27:13 +0100

    Updated user feed to accomodate recipes and meals

--- project/website/views.py
+++ project/website/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Michael',
-        'title': "Michael's First Post",
-        'content': 'I am a student at the University of Surrey and I am studying Computer Science.',
-        'date_posted': 'April 13, 2023'
-    },
-    {
-        'author': 'Nishanth',
-        'title': "Nishanth's First Post",
-        'content': 'I am a teacher at the University of Surrey and I am teaching Computer Science.',
-        'date_posted': 'January 12, 2023'
-    },
-]
+from .models import Meal, Recipe
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'meals': Meal.objects.all(),
+        'recipes': Recipe.objects.all(),
     }
     return render(request, 'website/home.html', context)
 
