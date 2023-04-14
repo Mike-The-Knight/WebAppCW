6e1ad72de67a598d10819dc87acfab092372e6b2
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-14 15:41:34 +0100

    Updated the signup form

--- project/members/views.py
+++ project/members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 
 # Create your views here.
 def account(request):
@@ -10,14 +10,14 @@ def account(request):
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('signin')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'members/signup.html', {'form': form})
 
 
@@ -33,7 +33,7 @@ def signin(request):
             return redirect("home")
         else:
             messages.error(request, "Incorrect username or password")
-            return redirect("account")
+            return redirect("signin")
 
     else:
         return render(request, 'members/signin.html', {'title': 'Sign in'})
