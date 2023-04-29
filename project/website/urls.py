292ad67a8a1f1520b4f9afa3c8aaa078e4e7486e
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-29 10:38:15 +0100

    Created class views for Meals and Recipes

--- project/website/urls.py
+++ project/website/urls.py
@@ -17,6 +17,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
 ]
--- project/website/views.py
+++ project/website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Meal, Recipe
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
 def home(request):
     context = {
@@ -11,3 +11,47 @@ def home(request):
 
 def about(request):
     return render(request, 'website/about.html', {'title': 'About'})
+
+# Class views for Meals and Recipes
+class MealListView(ListView):
+    model = Meal
+    template_name = 'website/home.html'
+    context_object_name = 'meals'
+    ordering = ['-date_posted']
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'website/home.html'
+    context_object_name = 'recipes'
+    ordering = ['-date_posted']
+
+class MealDetailView(DetailView):
+    model = Meal
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+
+class MealCreateView(CreateView):
+    model = Meal
+    fields = ['title', 'content']
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    fields = ['title', 'content']
+
+class MealUpdateView(UpdateView):
+    model = Meal
+    fields = ['title', 'content']
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    fields = ['title', 'content']
+
+class MealDeleteView(DeleteView):
+    model = Meal
+    success_url = '/'
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    success_url = '/'
+
