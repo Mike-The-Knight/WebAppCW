05537f191f743b0095fa5b18c4f120a5842cf389
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-04-30 11:32:10 +0100

    Updated save function

--- project/members/models.py
+++ project/members/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,3 +13,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
