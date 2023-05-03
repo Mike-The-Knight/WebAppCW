cda9068ca8205496dbde4075ab7fee379176029a
Robyn <77750040+penguin0001@users.noreply.github.com>
2023-05-03 12:19:14 +0100

    Delete posts_service directory
    Redundant now we're not using microservices

--- posts_service/posts_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Comment)
-admin.site.register(Review)
