f4ea22730f0e555a56f3ea7369778ef3a00c8e6c
Mike-The-Knight <michaelbawhitenowh@gmail.com>
2023-05-07 21:55:19 +0100

    Database integration with docker

--- project/project/settings.py
+++ project/project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MealMateDB',
+        'USER': 'MichaelBAW',
+        'PASSWORD': 'MBAW2505',
+        'HOST': 'db',  # This is the name of the service defined in docker-compose.yml
+        'PORT': '3306',
     }
 }
 
